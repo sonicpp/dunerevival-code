c67da65733cd551e5f87b584a8f9490e7a110c2e
Pierre Clément <pierroelmito@41ff109a-1bf0-4861-b1ee-0bb79d619952>
2011-01-13 20:39:21 +0000

    * Add info messages
    git-svn-id: https://svn.code.sf.net/p/dunerevival/code/src/dunerevival/pierro_cpp_SDL@11 41ff109a-1bf0-4861-b1ee-0bb79d619952

--- DuneReloaded/Src/Gfx/DuneFont.cpp
+++ DuneReloaded/Src/Gfx/DuneFont.cpp
@@ -30,6 +30,7 @@ void CDuneFont::Load(const char* _szFileName)
 
 void CDuneFont::Print(SDL_Surface* _pkDest, Uint32 _iX, Uint32 _iY, tColor _iWhite, const char* _szText)
 {
+#if 1
 	Uint8* pcBuffer = m_kData.Data();
 
 	SDL_LockSurface(_pkDest);
@@ -42,20 +43,31 @@ void CDuneFont::Print(SDL_Surface* _pkDest, Uint32 _iX, Uint32 _iY, tColor _iWhi
 		Uint8 iW = m_iCharWidth[iChar];
 		Uint32 iO = m_iOffset[iChar];
 		
+		Uint8* pcLine = pcBuffer + 256 + iO;
+		tColor* piDestPixels = pPixels + _iY * _pkDest->w + _iX;
 		for (Uint32 y = 0; y < 9; ++y)
 		{
-			Uint8 iLine = pcBuffer[256 + iO + y];
-			for (Uint32 x = 0; x < iW; ++x)
+			Uint8 iLine = *pcLine++;
+			
+			if (iLine != 0)
 			{
-				bool bPlot = (iLine & (1 << (7 - x))) != 0;
-				if (bPlot)
-					pPixels[(_iY + y) * _pkDest->w + _iX + x] = _iWhite;
+				tColor* piDestLine = piDestPixels;
+				for (Uint32 x = 0; x < iW; ++x)
+				{
+					bool bPlot = (iLine & (1 << (7 - x))) != 0;
+					if (bPlot)
+						*piDestLine = _iWhite;
+					++piDestLine;
+				}
 			}
+
+			piDestPixels += _pkDest->w;
 		}
 		_iX += iW;
 	}
 
 	SDL_UnlockSurface(_pkDest);
+#endif
 }
 
 }
