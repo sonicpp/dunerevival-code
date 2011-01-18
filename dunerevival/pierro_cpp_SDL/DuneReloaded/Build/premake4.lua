8ff7328a931fecf053c54cc072a3182f887bb42f
Monsieur OUXX <monsieurouxx@41ff109a-1bf0-4861-b1ee-0bb79d619952>
2011-01-18 13:22:45 +0000

    git-svn-id: https://svn.code.sf.net/p/dunerevival/code/src@22 41ff109a-1bf0-4861-b1ee-0bb79d619952

--- dunerevival/pierro_cpp_SDL/DuneReloaded/Build/premake4.lua
+++ dunerevival/pierro_cpp_SDL/DuneReloaded/Build/premake4.lua
@@ -1,27 +1,45 @@
 
 solution "DuneR"
 
+	location (_ACTION) --creates a "./vs2008" subfolder, or "./vs2010", etc. and places the solution files there
 	configurations { "Debug", "Release" }
-	includedirs { "../../../usr/include", "../../Common/Src/" }
-	libdirs { "../../../usr/lib" }
+	
+	--includedirs { "../../../usr/include", "../../Common/Src/" }
+	--libdirs { "../../../usr/lib" }
+	
+	
+	--strictly SDL
+	includedirs { "../../../../../external_libs/SDL/include/SDL"}
+	libdirs { "../../../../../external_libs/SDL/lib" }
 
-	configuration "vs*"
+	--additional SDL extensions
+	includedirs { "../../../../../external_libs/SDL_image/include"}
+	libdirs { "../../../../../external_libs/SDL_image/lib" }
+	includedirs { "../../../../../external_libs/SDL_mixer/include"}
+	libdirs { "../../../../../external_libs/SDL_mixer/lib" }
+	includedirs { "../../../../../external_libs/SDL_ttf/include"}
+	libdirs { "../../../../../external_libs/SDL_ttf/lib" }
+	
+	
+	
+
+	configuration "vs2008"
 		defines { "WIN32", "_WIN32", "WIN32_LEAN_AND_MEAN", "_CRT_SECURE_NO_WARNINGS" }
 
+
+	configuration "vs2010"
+		defines { "WIN32", "_WIN32", "WIN32_LEAN_AND_MEAN", "_CRT_SECURE_NO_WARNINGS" }
+
+		
 	configuration "codeblocks"
 		--defines { "WIN32", "_WIN32", "WIN32_LEAN_AND_MEAN", "_CRT_SECURE_NO_WARNINGS" }
+		links { "mingw32" }
 		
 	configuration "linux"
 		defines { "_LINUX" }
+		
+	--configuration "windows"
 
-	configuration "windows"
-		--for SDL strictly
-		includedirs { "C:/SDL-1.2.14/include/SDL/" }
-		libdirs { "C:/SDL-1.2.14/lib/" }
-
-		--for SDL_mixer
-		-- includedirs { "C:/SDL-1.2.14/include/SDL/" } --SDL_mixer.h should be copied into that folder, already included above
-		--libdirs { "C:/SDL-1.2.14/lib/" } --SDL_mixer.lib should be copied into that folder, already included above
 		
 	configuration "Debug"
 		defines { "DEBUG" }
@@ -35,9 +53,18 @@ solution "DuneR"
 
 		kind "ConsoleApp"
 		language "C++"
+		
+		--where the excutable will be created
 		targetdir "../Data"
-		links { "SDL", "SDLmain" }
+		
+		--working directory
+		-- "../Data" 	--This command DOES NOT EXIST YET in Premake but is under discussion at http://industriousone.com/post/set-working-directory
+						--at the moment it has to be changed manually
+		
+		links { "SDLmain", "SDL"} --do not revert the order
+		links {"SDL_image", "SDL_mixer", "SDL_ttf" } 
 
-		includedirs { "../Src/", "../Src/Game", "../Src/Gfx", "../Src/Core" }
+		--project itself
+		includedirs { "../Src" , "../Src/Core" , "../Src/Game" , "../Src/Gfx"}
 
 		files { "../Src/**.h", "../Src/**.cpp" }
